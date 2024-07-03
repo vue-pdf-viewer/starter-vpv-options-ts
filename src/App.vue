02dac914e48f20a623b909bcdfd00c7a5a0586ba
haroonth <kittisak.ma@logicspark.com>
2024-07-03 17:50:21 +0700

    ✨ Add VPdfViewer examples

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <script lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-
+  import { VPdfViewer, type ToolbarOptions } from '@vpv/viewer';
+  const toolbarOptions: Partial<ToolbarOptions> | false = false
 export default {
-  components: { HelloWorld }
+  components: { VPdfViewer }
 }
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>VPV Starter Toolkit: Vue + Options API + TypeScript</h1>
+    <br />
+    <h2>Default Toolbar</h2>
+    <div class="pdf-viewer-wrapper">
+      <VPdfViewer
+        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+    </div>
+    <h2>Without Toolbar</h2>
+    <div class="pdf-viewer-wrapper">
+      <VPdfViewer src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+        :toolbar-options="toolbarOptions" />
+    </div>
+    <h2>Mobile</h2>
+    <div class="pdf-viewer-wrapper-mobile">
+      <VPdfViewer
+        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .pdf-viewer-wrapper {
+    width: 1028px;
+    height: 700px;
+    margin: 0 auto;
+  }
+
+  .pdf-viewer-wrapper-mobile {
+    width: 600px;
+    height: 700px;
+    margin: 0 auto;
+  }
 </style>
